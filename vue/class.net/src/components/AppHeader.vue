ae8e47c20283509d5247592f3a1d7ab143738183
pavels-k <pavelrusk@gmail.com>
2020-04-11 20:26:14 +0300

    First commit

---
+++ vue/class.net/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app> 
+        <v-toolbar app class="primary">
+            <v-toolbar-title v-text="'class.net'"> </v-toolbar-title>
+        </v-toolbar>
+    </v-app>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
